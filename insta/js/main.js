fa4e3f7856138af3360327723b2cb575987f5e37
TH Chan <th.chan@accenture.com>
2022-11-16 17:24:48 +0800

    [Insta] add official API test

--- insta/js/main.js
+++ insta/js/main.js
@@ -22,4 +22,30 @@ $(function () {
 
         console.log('test ', $(this));
     });
+
+
+    // official instagram graph API
+    $.ajax({
+      url: "https://graph.instagram.com/me/media",
+      type: "get", //send it through get method
+      data: { 
+        fields: caption,id,media_type,media_url,permalink,thumbnail_url,timestamp,username, 
+        access_token: IGQVJYUXJuc2ZAGcnhGdTFZAQjJzR0dvT2J1YUlLMXN5YXNwT25RRG96SjhXWFltdXNPdEdJWE1VSXVOMzIyTkgyOVA2QzNZAcVh2ci11b1VUNEZARM00wakEwY1ZAYdm1TNW9wQm00Vl93
+      },
+      success: function(response) {
+        //Do Something
+        console.log('official success: ', response);
+        let instaItems = '';
+        response.data.forEach((value, index) => {
+          if (index <= 3) {
+            instaItems += '<div class="insta-item"><a href="' + value.permalink + '" target="_blank"><img src="' + value.media_url + '" /></a></div>';
+          }
+        });
+        $('#insta-post-wrapper').append(instaItems);
+      },
+      error: function(xhr) {
+        //Do Something to handle error
+        console.log('official fail: ', xhr);
+      }
+    });
 });
